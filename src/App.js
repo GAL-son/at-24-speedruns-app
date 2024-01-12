f2c3565b35838cd2667a3ffe9b0a90d572b5a5f1
GAL_son <pancerek1@gmail.com>
2024-01-12 19:14:37 +0100

    Basic layout setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import Layout from './pages/layout';
+import ErrorPage from './pages/error';
+
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <div>HELLO WORLD</div>},
+    { path: "/", element: <Layout/>, errorElement: <ErrorPage/>},
   ])
 
   return (
