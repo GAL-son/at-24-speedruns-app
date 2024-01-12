4927c00c838384655643ee09a94c643a075f1769
GAL_son <pancerek1@gmail.com>
2024-01-12 20:33:05 +0100

    Created Navbar Component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import './css/navbar.css'
+
+export default function Navbar() {
+
+    return(
+        <nav class="navbar navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+    )
+}
