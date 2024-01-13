797bf3a96d5354a268ad1be1380138dbd7702ca6
GAL_son <pancerek1@gmail.com>
2024-01-13 14:22:46 +0100

    Home Route

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+
+
+export default function Home() {
+
+
+    return(
+        <div className="d-flex flex-row">
+
+            <div className="main">
+                MAIN CONTENT 
+            </div>
+
+            <div className="side">
+                SIDE CONTENT
+            </div>
+
+        </div>
+    )
+}
