1fce49eed034859d02847d34a88b31a9269a963d
GAL_son <pancerek1@gmail.com>
2024-01-13 14:32:16 +0100

    Home page initial layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,6 @@
-
+import Footer from '../components/footer'
+import List from '../components/list'
+import './css/home.css'
 
 export default function Home() {
 
@@ -6,12 +8,24 @@ export default function Home() {
     return(
         <div className="d-flex flex-row">
 
-            <div className="home-main">
-                MAIN CONTENT 
+            <div className="home-main me-3 d-flex flex-column">
+                <div>
+                    New entries
+                    <div>
+                        <List content={[1,2,3,4,5]} Item={Footer}/>
+                    </div>
+                </div> 
             </div>
 
-            <div className="side">
-                SIDE CONTENT
+            <div className="side d-flex flex-column">
+                <div>
+                    Popular games
+
+                    
+                </div> 
+                <div>
+                    Most active users
+                </div> 
             </div>
 
         </div>
